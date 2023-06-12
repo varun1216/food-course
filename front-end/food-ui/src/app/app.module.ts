618bbb518ab7e5469cc0a2c0d50941e68e1a4221
Varun Raj <varun@hyreo.com>
2023-06-12 14:58:50 +0530

    New pages and service creation

--- front-end/food-ui/src/app/app.module.ts
+++ front-end/food-ui/src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+import { OrderTrackPageComponent } from './components/pages/order-track-page/order-track-page.component';
+import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoadingComponent,
+    CartPageComponent,
+    FoodPageComponent,
+    HomeComponent,
+    LoginPageComponent,
+    CheckoutPageComponent,
+    OrderTrackPageComponent,
+    PaymentPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
